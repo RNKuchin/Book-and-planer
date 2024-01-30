5b5b08bdc82cacb4e7fa5465c16e8513c472dd66
Roman Kuchin <roman.kuchin@mail.com>
2024-01-30 15:30:08 +0500

    refact: navMenu js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,101 +1,27 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/js/modules/mobile-nav.js":
-/*!**************************************!*\
-  !*** ./src/js/modules/mobile-nav.js ***!
-  \**************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
-/* harmony export */ });
-function mobileNav() {
-  // Mobile nav button
-  const navBtn = document.querySelector('.mobile-nav-btn');
-  const nav = document.querySelector('.nav');
-  const menuIcon = document.querySelector('.nav-icon');
-  navBtn.onclick = function () {
-    nav.classList.toggle('mobile-nav--open');
-    menuIcon.classList.toggle('nav-icon--active');
-    //document.body.classList.toggle('no-scroll');
-    document.querySelector('html').classList.toggle('no-scroll');
-  };
-}
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (mobileNav);
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _modules_mobile_nav_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/mobile-nav.js */ "./src/js/modules/mobile-nav.js");
-
-(0,_modules_mobile_nav_js__WEBPACK_IMPORTED_MODULE_0__["default"])();
-})();
-
+const mobileNav = () => {
+  const navBtn = document.querySelector('.mobile-nav-btn');
+  const nav = document.querySelector('.nav');
+  const menuIcon = document.querySelector('.nav-icon');
+  const htmlDocument = document.querySelector('html');
+  nav.classList.toggle('mobile-nav--open');
+  menuIcon.classList.toggle('nav-icon--active');
+  htmlDocument.classList.toggle('no-scroll');
+  nav.addEventListener('click', e => {
+    const navList = nav.querySelector('.nav__list');
+    const navLink = nav.querySelector('.nav__link');
+    if (e.target === navList || navLink) {
+      nav.classList.remove('mobile-nav--open');
+      menuIcon.classList.remove('nav-icon--active');
+      htmlDocument.classList.remove('no-scroll');
+    }
+  });
+};
+document.querySelector('.mobile-nav-btn').addEventListener('click', () => mobileNav());
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,24 @@
-import mobileNav from './modules/mobile-nav.js'
-mobileNav()
+const mobileNav = () => {
+	const navBtn = document.querySelector('.mobile-nav-btn')
+	const nav = document.querySelector('.nav')
+	const menuIcon = document.querySelector('.nav-icon')
+	const htmlDocument = document.querySelector('html')
+
+	nav.classList.toggle('mobile-nav--open')
+	menuIcon.classList.toggle('nav-icon--active')
+	htmlDocument.classList.toggle('no-scroll')
+
+	nav.addEventListener('click', e => {
+		const navList = nav.querySelector('.nav__list')
+		const navLink = nav.querySelector('.nav__link')
+
+		if (e.target === navList || navLink) {
+			nav.classList.remove('mobile-nav--open')
+			menuIcon.classList.remove('nav-icon--active')
+			htmlDocument.classList.remove('no-scroll')
+		}
+	})
+}
+document
+	.querySelector('.mobile-nav-btn')
+	.addEventListener('click', () => mobileNav())
